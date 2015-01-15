4e9cbf2ea9675c58b353f9693b8c90e9376473dd
blockbot <blockbot@gmail.com>
2015-01-15 09:39:40 -0700

    removed foundation, added sass breakpoint file, added infinite scroll/lazyload option, configured jquery shim, added body class function, updated html structure, added js baseurl

--- library/js/main.js
+++ library/js/main.js
@@ -1,5 +1,58 @@
-require(["interface/global"], function(){
+var footer = document.getElementsByTagName("footer"),
+	templateURL = footer[0].attributes[0].value;
+
+require.config({
+    baseUrl: templateURL + "/library/js/",
+    paths: {
+        jquery: 'vendor/jquery.min'
+    }
+});
+
+require(["jquery","interface/global"], function($, global){
 
 	console.log("global loaded");
+
+	//
+	// Lazyload and infinite scroll if wanted. Probably need to update for each specific site.
+	//
+
+	
+	// if($(".blog")){
+
+	// 	require(["vendor/jquery.infinitescroll.min"], function(infinitescroll){
+			
+	// 		$('.posts').infinitescroll({
+				
+ // 				loading: {
+	// 				finishedMsg: "<p><em>No more posts.</em></p>",
+	// 				img: "/wp-content/themes/jilldehnert/img/loading.gif",
+	// 				msgText: "Loading"
+	// 			},
+	// 		    navSelector: "div.next-posts",            
+	// 		    nextSelector: "div.next-posts a:first",    
+	// 		    itemSelector: ".posts div.post" 
+
+	// 		  	}, function(){
+
+	// 			  	$("img.lazy").lazyload({
+	// 					effect : "fadeIn"
+	// 				});
+
+	// 				// fire GA tracking on loading new set of posts
+	// 				_gaq.push(["_trackPageview", window.location.href]);
+
+	// 		});
+			
+	// 	});
+
+	// }
+
+	// require(["vendor/jquery.lazyload.min"], function(lazyload){
+
+	// 	$(".lazy").lazyload({
+	// 		effect : "fadeIn"
+	// 	});
+
+	// });
 
 });
